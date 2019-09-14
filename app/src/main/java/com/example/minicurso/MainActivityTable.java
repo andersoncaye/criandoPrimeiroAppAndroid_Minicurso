042b97e4d25228cf6d2a806635f1c40411bf91cc
Anderson Caye <andersoncaye@gmail.com>
2019-09-13 21:04:05 -0300

    Init

---
+++ app/src/main/java/com/example/minicurso/MainActivityTable.java
@@ -0,0 +1,13 @@
+package com.example.minicurso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_table);
+    }
+}
