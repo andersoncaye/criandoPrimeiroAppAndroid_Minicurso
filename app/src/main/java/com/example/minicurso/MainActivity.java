ee9ef44296c8b97da2c2d7a24c8e8c2a0f996857
Anderson Caye <andersoncaye@gmail.com>
2019-10-02 23:48:43 -0300

    Finalizado App

--- app/src/main/java/com/example/minicurso/MainActivity.java
+++ app/src/main/java/com/example/minicurso/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.minicurso;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Verificar se a permissão foi concedida
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            return;
+        }
+
         lista = DAOArquivo.carregarArquivo();
 
         btSalvar = (Button) findViewById(R.id.bt_salvar);
@@ -45,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                     if (situacao){
                         tvContador.setText("Total de itens cadastrados "+lista.size());
                         lista = temp;
+                        ptCampoItem.setText("");
                         Toast.makeText(getApplicationContext(), "Item salvo com sucesso!", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(getApplicationContext(), "Ops! Erro ao salvar o item!", Toast.LENGTH_SHORT).show();
@@ -61,7 +71,9 @@ public class MainActivity extends AppCompatActivity {
                 it.putStringArrayListExtra("lista", lista);
 //                Bundle bu = new Bundle();
 //                bu.putStringArrayList("lista", lista);
+//                it.putExtra(bu);
                 startActivity(it);
+                finish();
             }
         });
 
--- app/src/main/java/com/example/minicurso/MainActivityTable.java
+++ app/src/main/java/com/example/minicurso/MainActivityTable.java
@@ -1,13 +1,71 @@
 package com.example.minicurso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.minicurso.Class.DAOArquivo;
+
+import java.util.ArrayList;
 
 public class MainActivityTable extends AppCompatActivity {
+
+    ArrayList<String> lista;
+    ListView lvItens;
+    Button btVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_table);
+
+        lvItens = (ListView) findViewById(R.id.lv_itens);
+        btVoltar = (Button) findViewById(R.id.bt_voltar);
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivityTable.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
+
+        Intent it = getIntent();
+        lista = it.getStringArrayListExtra("lista");
+
+        caregarListView();
+
+        lvItens.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                lista.remove(position);
+
+                if( DAOArquivo.salvarArquivo(lista) ){
+                    caregarListView();
+                    Toast.makeText(getApplicationContext(), "Item removido com sucesso!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Ops! Erro ao remover o item!", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    private void caregarListView(){
+        String[] dados = new String[lista.size()];
+        for ( int i = 0; i < lista.size(); i++){
+            dados[i] = lista.get(i);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
+        lvItens.setAdapter(adapter);
     }
 }
